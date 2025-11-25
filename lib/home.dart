9801cce2f839a0cd424717a6b04a7317d30449eb
labiba <labibahassan74@gmail.com>
2025-11-25 16:39:12 +0300

    first push

--- lib/home.dart
+++ lib/home.dart
@@ -12,34 +12,42 @@ class _HomeState extends State<Home> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("data"),
+
       ),
-        body: SafeArea(child: Row(
-          children: [
-            Column(
+        body: SafeArea(child:
+            Stack(
+              alignment: Alignment.center,
               children: [
-                Container(
-                  height: 300,
-                  width:200,
-                  color: const Color(0xFFF2F8FD),
-                  child: Column(
-                    children: [
-                      Text('Hi name'),
-                      Text('what sevice do you need?'),
-                      ElevatedButton(onPressed:
-                          (){}, child: Text('Get Start'))
-                    ],
-                  ),
-                )
+
+                Row(
+                  children: [
+                    Column(
+                      children: [
+                        Container(
+                          height: 300,
+                          width:200,
+                          color: const Color(0xFFF2F8FD),
+                          child: Column(
+                            children: [
+                              Text('Hi name'),
+                              Text('what service do you need?'),
+                              ElevatedButton(onPressed:
+                                  (){}, child: Text('Get Start'))
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      height: 10,
+                      width: 0,
+                    ),
+                    Image.asset('images1/photo_2025-11-21_21-18-43.jpg',width: 200,height: 200,),
+                  ],
+                ),
               ],
-            ),
-            SizedBox(
-              height: 10,
-              width: 0,
-            ),
-            Image.asset('images1/photo_2025-11-21_21-18-43.jpg',width: 200,height: 200,),
-          ],
-        ),)
+            )
+        )
     );
   }
 }
