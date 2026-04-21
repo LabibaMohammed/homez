b720bda2711afd63f3a9c6a62c058221c114a50c
labiba <labibahassan74@gmail.com>
2026-04-21 19:37:37 +0300

    `Updated Home page to use ConvexAppBar and added navigation to CategoryServicesPage`

--- lib/indexpage.dart
+++ lib/indexpage.dart
@@ -1,11 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:convex_bottom_bar/convex_bottom_bar.dart';
 import 'package:homez/account.dart';
 import 'package:homez/home.dart';
 import 'package:homez/request.dart';
 import 'package:homez/search.dart';
 
 class Indexpage extends StatefulWidget {
-  const Indexpage({super.key, required String firstName, required String lastName, required String email});
+  const Indexpage({
+    super.key,
+    required String firstName,
+    required String lastName,
+    required String email,
+  });
 
   @override
   State<Indexpage> createState() => _IndexpageState();
@@ -14,7 +20,7 @@ class Indexpage extends StatefulWidget {
 class _IndexpageState extends State<Indexpage> {
   int _bottomNavbar = 0;
   final List<Widget> _pages = [
-    Home(firstName: '', lastName: '', email: '',),
+    Home(firstName: '', lastName: '', email: ''),
     Search(),
     Request(),
     Account(name: "name", email: " "),
@@ -24,26 +30,22 @@ class _IndexpageState extends State<Indexpage> {
   Widget build(BuildContext context) {
     return Scaffold(
       body: IndexedStack(index: _bottomNavbar, children: _pages),
-      bottomNavigationBar: BottomNavigationBar(
-        currentIndex: _bottomNavbar,
-        onTap: (index) {
+      bottomNavigationBar: ConvexAppBar(
+        style: TabStyle.react,
+        backgroundColor: Colors.white,
+        activeColor: const Color(0xFF093A61),
+        color: Colors.grey[600],
+        initialActiveIndex: _bottomNavbar,
+        onTap: (int index) {
           setState(() {
             _bottomNavbar = index;
           });
         },
-        unselectedItemColor: Colors.grey[600],
-        selectedItemColor: const Color(0xFF093A61),
-        items: [
-          BottomNavigationBarItem(label: 'Home', icon: Icon(Icons.home)),
-          BottomNavigationBarItem(label: 'Search', icon: Icon(Icons.search)),
-          BottomNavigationBarItem(
-            label: 'Request',
-            icon: Icon(Icons.request_page),
-          ),
-          BottomNavigationBarItem(
-            label: 'Account',
-            icon: Icon(Icons.account_box),
-          ),
+        items: const [
+          TabItem(icon: Icons.home, title: 'Home'),
+          TabItem(icon: Icons.search, title: 'Search'),
+          TabItem(icon: Icons.request_page, title: 'Request'),
+          TabItem(icon: Icons.account_box, title: 'Account'),
         ],
       ),
     );
